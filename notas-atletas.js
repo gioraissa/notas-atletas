05c813ed39d574c702355ce13101684c8fbba388
gioraissa <raissagiovanna770@gmail.com>
2025-10-19 18:30:19 -0300

    Create notas-atletas.js
    projeto final

---
+++ notas-atletas.js
@@ -0,0 +1,27 @@
+let atletas = [
+  { nome: "Cesar Abascal", notas: [10, 9.34, 8.42, 10, 7.88] },
+  { nome: "Fernando Puntel", notas: [8, 10, 10, 7, 9.33] },
+  { nome: "Daiane Jelinsky", notas: [7, 10, 9.5, 9.5, 8] },
+  { nome: "Bruno Castro", notas: [10, 10, 10, 9, 9.5] }
+];
+
+function calcularMedias(objAtletas) {
+  for (let i = 0; i < objAtletas.length; i++) {
+    let atleta = objAtletas[i];
+    let notas = atleta.notas.slice();
+    notas.sort((a, b) => a - b);
+    let notasComputadas = notas.slice(1, 4);
+    let soma = 0;
+    notasComputadas.forEach(nota => {
+      soma += nota;
+    });
+    let media = soma / notasComputadas.length;
+
+    console.log(`Atleta: ${atleta.nome}`);
+    console.log(`Notas Obtidas: ${atleta.notas.join(",")}`);
+    console.log(`Média Válida: ${media.toFixed(3)}`);
+    console.log("");
+  }
+}
+
+calcularMedias(atletas);
